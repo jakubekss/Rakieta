2dec08a112efbcadec95489f92dcaecd9e36a5e6
Jakub <61504210+jakubekss@users.noreply.github.com>
2020-05-13 01:28:23 +0200

    Update aplikacja.py
    Gotowe GUI bez funkcji

--- aplikacja.py
+++ aplikacja.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QLineEdit, QPushButton
 import sys
 
 class Aplikacja(QWidget):
@@ -11,21 +11,58 @@ class Aplikacja(QWidget):
         # etykiety
         etykieta1 = QLabel("Masa rakiety w kg", self)
         etykieta2 = QLabel("Średnica rakiety w m", self)
-        etykieta3 = QLabel("Współczynnik oporu rakiety", self)
+        etykieta3 = QLabel("Współczynnik oporu aerodynamicznego", self)
+        etykieta4 = QLabel("Impuls silnika w Ns", self)
+        etykieta5 = QLabel("Ciąg rakiety w N", self)
+        etykieta6 = QLabel("Pułap na który wzniesie się rakieta w m", self)
 
-        # przypisanie widgetów do układu tabelarycznego
+        # przypisanie widgetów do układu tabeli
         ukladT = QGridLayout()
         ukladT.addWidget(etykieta1, 0, 0)
         ukladT.addWidget(etykieta2, 1, 0)
         ukladT.addWidget(etykieta3, 2, 0)
+        ukladT.addWidget(etykieta4, 3, 0)
+        ukladT.addWidget(etykieta5, 4, 0)
+        ukladT.addWidget(etykieta6, 5, 0)
+
+        # 1-liniowe pola edycyjne
+        self.masa = QLineEdit()
+        self.srednica = QLineEdit()
+        self.opor = QLineEdit()
+        self.impuls = QLineEdit()
+        self.ciag = QLineEdit()
+        self.wynik = QLineEdit()
+        
+        self.wynik.setDisabled(True)
+        
+        self.masa.setToolTip('Wpisując masę używaj kropki')
+        self.srednica.setToolTip('Wpisując średnicę używaj kropki')
+        self.opor.setToolTip('Wpisując wspólczynnik oporu używaj kropki')
+
+        ukladT.addWidget(self.masa, 0, 1)
+        ukladT.addWidget(self.srednica, 1, 1)
+        ukladT.addWidget(self.opor, 2, 1)
+        ukladT.addWidget(self.impuls, 3, 1)
+        ukladT.addWidget(self.ciag, 4, 1)
+        ukladT.addWidget(self.wynik, 5, 1)
+
+        # przyciski
+        obliczBtn = QPushButton("&Oblicz", self)
+        obliczBtn.resize(obliczBtn.sizeHint())
+
+        zamknijBtn = QPushButton("&Zamknij", self)
+        zamknijBtn.clicked.connect(quit)
+
+        ukladT.addWidget(obliczBtn, 6, 0, 1, 2)
+        ukladT.addWidget(zamknijBtn, 7, 1)
 
         # przypisanie utworzonego układu do okna
         self.setLayout(ukladT)
+    
 
-        self.resize(300, 100)
+        self.resize(300, 150)
         self.setWindowTitle("Rakieta")
         self.show()
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
